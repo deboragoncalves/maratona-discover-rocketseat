c93a332efa3fcc272df703d49b1e0f14ff6b0b85
deboracristinapereira123@gmail.com <deboracristinapereira123@gmail.com>
2021-03-27 20:55:29 -0300

    Get e instalacao nodemon - Criar servidor

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const server = express();
 
-server.get("/", () => console.log("enter"));
+server.get("/", (request, response) => {
+    console.log("enter");
+    return response.send("Hello World")
+});
 
 server.listen(3000, () => console.log("running server"));
