38a1b00d89fdcb08d7a59dfae8779d4e00922dbb
deboracristinapereira123@gmail.com <deboracristinapereira123@gmail.com>
2021-03-27 23:18:57 -0300

    Template Engine

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const routes = express.Router();
 
-const basePath = __dirname + "/views";
+const views = __dirname + "/views/";
 
-routes.get("/", (request, response) => response.sendFile(basePath + "/index.html"));
-routes.get("/job", (request, response) => response.sendFile(basePath + "/job.html"));
-routes.get("/job/edit", (request, response) => response.sendFile(basePath + "/job-edit.html"));
-routes.get("/profile", (request, response) => response.sendFile(basePath + "/profile.html"));
+routes.get("", (request, response) => response.render(views + "index.ejs"));
+routes.get("job", (request, response) => response.render(views + "job.ejs"));
+routes.get("jobedit", (request, response) => response.render(views + "job-edit.ejs"));
+routes.get("profile", (request, response) => response.render(views + "profile.ejs"));
 
 module.exports = routes;
