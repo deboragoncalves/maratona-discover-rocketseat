561dc9cd39ceb1906ff8bf1c3cc83d529983e66c
deboracristinapereira123@gmail.com <deboracristinapereira123@gmail.com>
2021-04-04 02:21:42 -0300

    Valor hora

--- src/routes.js
+++ src/routes.js
@@ -18,8 +18,27 @@ const Profiles = {
   controllers: {
       updateProfile: (request, response) => response.render(views + "profile.ejs", { profile: Profiles.data }),
 
-      update: () => {
+      profileCreate: (request, response) => {
+        
+        const dataProfile = request.body;
 
+        const weeksYear = 52;
+
+        const weeksMonth = (weeksYear - dataProfile["vacation-per-year"]) / 12;
+
+        // Horas trabalhadas semana e mês
+
+        const workHoursWeek = dataProfile["days-per-week"] * dataProfile["hours-per-day"];
+
+        const workHoursMonth = workHoursWeek * weeksMonth;
+
+        dataProfile["value-hour"] = dataProfile["monthly-budget"] / workHoursMonth;
+
+        // Atualizar data
+
+        Profiles.data = dataProfile;
+
+        return response.redirect('/profile');
       },
   }
 };
@@ -126,5 +145,6 @@ routes.get("/job/edit", (request, response) =>
   response.render(views + "job-edit.ejs")
 );
 routes.get("/profile", Profiles.controllers.updateProfile);
+routes.post("/profile", Profiles.controllers.profileCreate);
 
 module.exports = routes;
